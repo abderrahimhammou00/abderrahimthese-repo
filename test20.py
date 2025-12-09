3ee02421a7d5e06f92e131860dcaba6104209589
abderrahimhammou00 <abderrahimhammou00@gmail.com>
2025-12-09 10:15:50 +0100

    Update test20.py
    Correction du code

--- test20.py
+++ test20.py
@@ -1,3 +1,26 @@
-#includ<stdio.h>
-    main()
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Chargement des données
+data1 = np.loadtxt('Int.txt')
+#Colonnes : Z, SED NR, SED NR+WC, SED SR, SED SR+WC
+Z = data1[:, 0]
+SED1 = data1[:, 1]
+
+
+# Tracé
+plt.plot(Z, SED1, color='red', label=r'$\frac{d\sigma}{d\Omega}|_{NR}$')
+
+# Axes et légendes
+plt.xlabel(r'$Z$', fontsize=12)
+plt.ylabel(r'$\frac{d\sigma}{d\Omega} \, (a.u.)$', fontsize=12)
+plt.legend(loc='best')
+plt.grid(True)
+#plt.yscale('log') # décommente si nécessaire
+
+# Sauvegarde en PDF
+plt.savefig("SED.pdf", format="pdf", bbox_inches="tight")
+
+# Affichage
+plt.show()
     
